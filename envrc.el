purcell/envrc

commit 7effcda0c9870247da72e7bc56bcac2ca12fcbde

Update info-directory-list from INFOPATH if set

envrc.el
@@ -299,6 +299,7 @@ also appear in PAIRS."
   (with-current-buffer buf
     (kill-local-variable 'exec-path)
     (kill-local-variable 'process-environment)
+    (kill-local-variable 'info-directory-list)
     (when (derived-mode-p 'eshell-mode)
       (if (fboundp 'eshell-set-path)
           (eshell-set-path (butlast exec-path))
@@ -320,7 +321,9 @@ also appear in PAIRS."
         (when (derived-mode-p 'eshell-mode)
           (if (fboundp 'eshell-set-path)
               (eshell-set-path path)
-            (setq-local eshell-path-env path)))))))
+            (setq-local eshell-path-env path))))
+      (when-let ((info-path (getenv "INFOPATH")))
+        (setq-local Info-directory-list (parse-colon-path info-path))))))
 
 (defun envrc--update-env (env-dir)
   "Refresh the state of the direnv in ENV-DIR and apply in all relevant buffers."

commit efe6817162f1c9b9eb2d8b4c1a50cda70c3e8f1d

Let info.el parse INFOPATH
A trailing colon in INFOPATH indicates select default directories should
be appended to `Info-directory-list'. See `Info-default-dirs'.

`parse-colon-path' produces a trailing `nil' in `Info-directory-list'
when a trailing colon is present in INFOPATH. This leads to type errors
in Info functions expecting only strings.

`info-initialize' is called by Info entry points and parses INFOPATH
when `Info-directory-list' is not already set. Clear it to force parsing
instead of handling parsing ourselves.

Fixes: 7effcda0c987 ("Update info-directory-list from INFOPATH if set")
Closes: #25

envrc.el
@@ -322,8 +322,8 @@ also appear in PAIRS."
           (if (fboundp 'eshell-set-path)
               (eshell-set-path path)
             (setq-local eshell-path-env path))))
-      (when-let ((info-path (getenv "INFOPATH")))
-        (setq-local Info-directory-list (parse-colon-path info-path))))))
+      ;; tell info.el to parse INFOPATH again in case direnv modified it
+      (when (getenv "INFOPATH") (setq-local Info-directory-list nil)))))
 
 (defun envrc--update-env (env-dir)
   "Refresh the state of the direnv in ENV-DIR and apply in all relevant buffers."